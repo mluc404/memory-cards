6150a5442e6d9333b19eea144798c66482adfc4b
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-05-06 21:23:56 -0700

    feat: fetch and display 5 random pokemons on app load

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import { PokeList } from "./components/PokeList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <PokeList />
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,77 +1,40 @@
 import React, { useState, useEffect } from "react";
+import "../styles/PokeList.css";
 
 export function PokeList() {
-  const [pokeList, setPokeList] = useState([]);
+  const pokemonQuantity = 5;
+  const numArr = [];
+  const [pokemonList, setPokemonList] = useState([]);
 
-  const fetchPokemon = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
-    const data = await response.json();
-    setPokeList(data.results);
-  };
+  while (numArr.length < pokemonQuantity) {
+    const randNum = Math.floor(Math.random() * 100) + 1;
+    if (!numArr.includes(randNum)) numArr.push(randNum);
+  }
 
-  const [tenPoke, setTenPoke] = useState([]);
-  const getNewPokemon = () => {
-    const selectedPoke = [];
-    const ranNumArr = [];
-    while (selectedPoke.length < 10) {
-      const ranNum = Math.floor(Math.random() * 100);
-      if (!ranNumArr.includes(ranNum)) {
-        ranNumArr.push(ranNum);
-        selectedPoke.push(pokeList[ranNum]);
-      }
-    }
-    setTenPoke(selectedPoke);
+  const fetchData = async () => {
+    const promises = numArr.map(async (i) => {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+      return res.json();
+    });
+
+    const pokemons = await Promise.all(promises);
+    setPokemonList(pokemons);
   };
 
   useEffect(() => {
-    fetchPokemon()
+    fetchData();
   }, []);
-  
-  useEffect(()=>{
-    if(pokeList.length>0){
-      getNewPokemon();
-    }},[pokeList])
 
   return (
     <>
-      {/* <button onClick={fetchPokemon}>Fetch</button> */}
-      {/* <br></br> */}
-      <button onClick={getNewPokemon}> Get 10 Pokemons</button>
-      <ul>
-        {tenPoke.length === 10 &&
-          tenPoke.map((poke) => <li key={poke.name}>{poke.name}</li>)}
-      </ul>
+      <div className="pokemonList">
+        {pokemonList.map((pokemon) => (
+          <div key={pokemon.name} className="eachPokemon">
+            <p>{pokemon.name}</p>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
-
-// export function PokeList() {
-//   const [pokemon, setPokemon] = useState(null);
-
-//   useEffect(() => {
-//     const fetchPokemon = async () => {
-//       try {
-//         const response = await fetch(
-//           "https://pokeapi.co/api/v2/pokemon?limit=1"
-//         );
-//         const data = await response.json();
-//         console.log(data);
-//         setPokemon(data);
-//       } catch (error) {
-//         console.error("Error fetching pokemon:", error);
-//       }
-//     };
-
-//     fetchPokemon();
-//   }, []);
-
-//   return (
-//     <div>
-//       {pokemon ? (
-//         <pre>{JSON.stringify(pokemon, null, 2)}</pre>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// }
