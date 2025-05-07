7947bb5f4429d1f52e1ca87a57e75073fb8c917d
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-05-07 13:23:33 -0700

    feat: check clicked card matching orginal order

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -5,6 +5,8 @@ export function PokeList() {
   const numbersOfCards = 5;
   const numArr = [];
   const [pokemonList, setPokemonList] = useState([]);
+  const [prevList, setPrevList] = useState([]);
+  const [originalList, setOrgList] = useState([]);
 
   while (numArr.length < numbersOfCards) {
     const randNum = Math.floor(Math.random() * 100) + 1;
@@ -19,6 +21,7 @@ export function PokeList() {
 
     const pokemons = await Promise.all(promises);
     setPokemonList(pokemons);
+    setOrgList(pokemons);
   };
 
   useEffect(() => {
@@ -36,14 +39,37 @@ export function PokeList() {
     setPokemonList(newPokeList);
   };
 
+  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
+  const clickCard = (e) => {
+    if (count < pokemonList.length - 1) setCount((c) => c + 1);
+    score === 0 && setScore((s) => s + 1);
+
+    console.log(`count: ${count} ======================================`);
+
+    if (count > 0) {
+      console.log(`current id: ${e.currentTarget.id}`);
+      console.log(`prev id: ${originalList[count].name}`);
+      if (e.currentTarget.id === originalList[count].name) {
+        setScore((s) => s + 1);
+      }
+    }
+    shuffleCards();
+  };
+
   return (
     <>
+      <div className="count">Click: {count}</div>
+      <div className="score">Score: {score}</div>
       <div className="pokemonList">
         {pokemonList.map((pokemon) => (
           <div
             key={pokemon.name}
             className="eachPokemon"
-            onClick={shuffleCards}
+            id={pokemon.name}
+            onClick={(e) => {
+              clickCard(e);
+            }}
           >
             <p>{pokemon.name}</p>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
