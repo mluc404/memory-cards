c868f671bafb81b0a69a5b3b7ec48d3d86b702cd
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-05-07 17:44:32 -0700

    feat: add gameState management and handle fetch loading/error

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -2,34 +2,47 @@ import React, { useState, useEffect } from "react";
 import "../styles/PokeList.css";
 
 export function PokeList() {
-  const numbersOfCards = 5;
+  const numbersOfCards = 3;
   const numArr = [];
   const [pokemonList, setPokemonList] = useState([]);
-  const [prevList, setPrevList] = useState([]);
   const [originalList, setOrgList] = useState([]);
+  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameState, setGameState] = useState("playing");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  // Select random card ids to fetch
   while (numArr.length < numbersOfCards) {
     const randNum = Math.floor(Math.random() * 100) + 1;
     if (!numArr.includes(randNum)) numArr.push(randNum);
   }
 
+  // Function to fetch the cards using above ids
   const fetchData = async () => {
-    const promises = numArr.map(async (i) => {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-      return res.json();
-    });
-
-    const pokemons = await Promise.all(promises);
-    setPokemonList(pokemons);
-    setOrgList(pokemons);
+    setIsLoading(true);
+    try {
+      const promises = numArr.map(async (i) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        return res.json();
+      });
+      const pokemons = await Promise.all(promises);
+      setPokemonList(pokemons);
+      setOrgList(pokemons); // original list to check with every click later}
+    } catch (error) {
+      setError("Failed to load pokemons. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
+  // Upon component mounting, fetch the cards
   useEffect(() => {
     fetchData();
   }, []);
 
+  // Function to shuffle cards
   const shuffleCards = () => {
-    const ranNumArr = [];
     const newPokeList = [...pokemonList];
 
     for (let i = newPokeList.length - 1; i > 0; i--) {
@@ -39,8 +52,7 @@ export function PokeList() {
     setPokemonList(newPokeList);
   };
 
-  const [count, setCount] = useState(0);
-  const [score, setScore] = useState(0);
+  // Function to handle card click
   const clickCard = (e) => {
     if (count < pokemonList.length - 1) setCount((c) => c + 1);
     score === 0 && setScore((s) => s + 1);
@@ -51,7 +63,14 @@ export function PokeList() {
       console.log(`current id: ${e.currentTarget.id}`);
       console.log(`prev id: ${originalList[count].name}`);
       if (e.currentTarget.id === originalList[count].name) {
-        setScore((s) => s + 1);
+        const newScore = score + 1;
+        setScore(newScore);
+        console.log("score:", newScore, "arr length:", pokemonList.length);
+        if (newScore === pokemonList.length) {
+          setGameState("won");
+        }
+      } else {
+        setGameState("lost");
       }
     }
     shuffleCards();
@@ -59,23 +78,39 @@ export function PokeList() {
 
   return (
     <>
-      <div className="count">Click: {count}</div>
-      <div className="score">Score: {score}</div>
-      <div className="pokemonList">
-        {pokemonList.map((pokemon) => (
-          <div
-            key={pokemon.name}
-            className="eachPokemon"
-            id={pokemon.name}
-            onClick={(e) => {
-              clickCard(e);
-            }}
-          >
-            <p>{pokemon.name}</p>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="wrapper">
+          <div className="gameInfo">
+            <div className="count">Click: {count}</div>
+            <div className="score">Score: {score}</div>
+            {gameState !== "playing" &&
+              (gameState === "won" ? (
+                <div>You won!</div>
+              ) : (
+                <div>Game over!</div>
+              ))}
           </div>
-        ))}
-      </div>
+          <div className="cardList">
+            {pokemonList.map((pokemon) => (
+              <div
+                key={pokemon.name}
+                className="eachPokemon"
+                id={pokemon.name}
+                onClick={(e) => {
+                  clickCard(e);
+                }}
+              >
+                <p>{pokemon.name}</p>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
